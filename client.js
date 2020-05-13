c9fa7b3edcb317a9273345e01bb8b86290bcf583
FrankHu <frank.hu@perficient.com>
2020-05-13 14:42:58 +0800

    add request

---
+++ client.js
@@ -0,0 +1,87 @@
+// use net to simulate http client
+// 72:49 paused
+const net = require("net");
+
+class Request {
+  constructor(options) {
+    this.method = options.method || "GET";
+    this.host = options.host;
+    this.port = options.port || 80;
+    this.body = options.body || {};
+    this.path = options.path || "/";
+    this.headers = options.headers || {};
+    if (!this.headers["Content-Type"]) {
+      this.headers["Content-Type"] = "application/x-www-form-urlencoded";
+    }
+
+    if (this.headers["Content-Type"] === "application/json") {
+      this.bodyText = JSON.stringify(this.body);
+    } else if (
+      this.headers["Content-Type"] === "application/x-www-form-urlencoded"
+    ) {
+      this.bodyText = Object.keys(this.body)
+        .map((key) => `${key}=${encodeURIComponent(this.body[key])}`)
+        .join("&");
+    }
+
+    this.headers["Content-Length"] = this.bodyText.length;
+  }
+
+  toString() {
+    return `${this.method} ${this.path} HTTP/1.1\r\n${Object.keys(this.headers)
+      .map((key) => `${key}: ${this.headers[key]}`)
+      .join("\r\n")}\r\n\r\n${this.bodyText}\r\n`;
+  }
+
+  send(connection) {
+    console.log(this.toString());
+    return new Promise((resolve, reject) => {
+      if (connection) {
+        connection.write(this.toString());
+      } else {
+        connection = net.createConnection(
+          {
+            host: this.host,
+            port: this.port,
+          },
+          () => {
+            connection.write(this.toString());
+          }
+        );
+      }
+
+      connection.on("data", (data) => {
+        resolve(data.toString());
+        connection.end();
+      });
+      connection.on("error", (err) => {
+        reject(err);
+        connection.end();
+      });
+      connection.on("end", () => {
+        console.log("disconnected from server");
+      });
+    });
+  }
+}
+
+class Response {}
+
+void (async function () {
+  let request = new Request({
+    method: "POST",
+    host: "127.0.0.1",
+    port: "8088",
+    path: "/",
+    headers: {
+      ["X-Foo2"]: "customed",
+    },
+    body: {
+      name: "winter",
+      age: 18,
+    },
+  });
+
+  let response = await request.send();
+  console.log(response);
+})();
