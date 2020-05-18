6e4619cf479bd3e113ca23cd7a3638bdb9d3e44f
FrankHu <frank.hu@perficient.com>
2020-05-18 22:14:00 +0800

    0514 22：27

---
+++ state-machine.js
@@ -0,0 +1,17 @@
+function match(str) {
+  let foundA = false;
+  for (let c of str) {
+    if (c === "a") {
+      foundA = true;
+    } else if (foundA && c === "b") {
+      return true;
+    } else {
+      foundA = false;
+    }
+  }
+  return false;
+}
+
+const res = match("I am groabot");
+
+console.log(res);
